02207be4d58492a24d196acf20dfc116ef561943
neverbiasu <neverbiasu@outlook.com>
2024-11-30 18:02:40 +0800

    fix: Repair color mode err

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-100 dark:bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-lg font-bold">
+        <Link href="/" className="text-black dark:text-white text-lg font-bold">
           Home
         </Link>
-        <Link href="/portfolio" className="text-white text-lg font-bold">
+        <Link href="/portfolio" className="text-black dark:text-white text-lg font-bold">
           Portfolio
         </Link>
       </div>
--- src/app/portfolio/1/page.tsx
+++ src/app/portfolio/1/page.tsx
@@ -32,7 +32,6 @@ const GoingOutChecklist = () => {
     const updatedCheckboxes = checkboxes.map((checkbox) =>
       checkbox.id === id ? { ...checkbox, checked: !checkbox.checked } : checkbox
     );
-    // 将已确认的任务沉到最底部，取消确认的任务回到原来的位置
     updatedCheckboxes.sort((a, b) => {
       if (a.checked === b.checked) {
         return a.order - b.order;
@@ -44,7 +43,7 @@ const GoingOutChecklist = () => {
 
   return (
     <Layout>
-      <h1 className="text-center text-2xl text-white mb-5">Going Out Checklist</h1>
+      <h1 className="text-center text-2xl mb-5 dark:text-white">Going Out Checklist</h1>
       <div className="flex flex-col items-center">
         <div className="flex mb-5 items-center justify-center">
           <input
@@ -52,20 +51,20 @@ const GoingOutChecklist = () => {
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="Enter checkbox text here❤"
-            className="w-60 p-2 text-base text-gray-800 rounded border border-gray-300 mr-2 text-center"
+            className="w-60 p-2 text-base rounded border mr-2 text-center text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700 border-gray-300 dark:border-gray-600"
           />
           <button
             onClick={addCheckbox}
-            className="p-2 text-base rounded border-none bg-blue-500 text-white cursor-pointer"
+            className="p-2 text-base rounded border-none cursor-pointer bg-blue-500 text-white"
           >
             Add it!
           </button>
         </div>
-        <div className="w-full max-w-2xl p-4 rounded border border-gray-300 bg-gradient-to-r from-blue-100 to-blue-200">
+        <div className="w-full md:w-1/2 h-screen p-4 rounded border bg-gray-50 dark:bg-gray-800 border-gray-300 dark:border-gray-700 mx-auto">
           {checkboxes.map((checkbox) => (
             <div
               key={checkbox.id}
-              className={`flex items-center mb-2 justify-between p-2 rounded border border-gray-300 bg-gray-100 transition-all duration-300 ease-in-out transform ${checkbox.checked ? 'order-last' : ''}`}
+              className={`flex items-center mb-2 justify-between p-2 rounded border transition-all duration-300 ease-in-out transform ${checkbox.checked ? 'order-last' : ''} bg-gray-100 dark:bg-gray-700 border-gray-300 dark:border-gray-600`}
             >
               <input
                 type="checkbox"
@@ -74,13 +73,13 @@ const GoingOutChecklist = () => {
                 className="w-6 h-6 mr-2 flex-shrink-0 cursor-pointer transition-transform hover:scale-110"
               />
               <span
-                className={`flex-grow text-stone-800 text-left mx-5 break-words ${checkbox.checked ? 'line-through' : ''}`}
+                className={`flex-grow text-left mx-5 break-words ${checkbox.checked ? 'line-through' : ''} text-gray-800 dark:text-gray-200`}
               >
                 {checkbox.value}
               </span>
               <button
                 onClick={() => removeCheckbox(checkbox.id)}
-                className="p-1 text-sm rounded border-none bg-red-500 text-white cursor-pointer flex-shrink-0"
+                className="p-1 text-sm rounded border-none cursor-pointer flex-shrink-0 bg-red-500 text-white"
               >
                 Remove
               </button>
