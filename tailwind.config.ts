3bfd01cc970440b729136f70a04322f5f6051d00
neverbiasu <neverbiasu@outlook.com>
2024-12-02 23:46:34 +0800

    fix: Update Baidu page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      backgroundColor: {
+        'baidu-blue': '#4e6ef2'
+      },
+      borderColor: {
+        'baidu-blue': '#4e6ef2',
+        'baidu-gray': '#c5c7ce'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
