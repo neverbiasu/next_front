98ca1acee66bdef2e4acf3bb402f6ffe19402a75
neverbiasu <neverbiasu@outlook.com>
2025-02-10 14:41:37 +0800

    style: Enhance portfolio layout and styling

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -22,16 +22,19 @@ const Portfolio = async () => {
 
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">Portfolio</h1>
-      <ul>
+      <h1 className="text-4xl font-bold mb-4">Portfolio</h1>
+      <div className="flex flex-wrap gap-4 justify-start mt-10">
         {directories.map((dir) => (
-          <li key={dir}>
-            <Link href={`/portfolio/${dir}`} className="text-blue-500 hover:underline">
+          <div
+            key={dir}
+            className="border p-4 rounded shadow-md w-auto h-1/12 transform transition-transform duration-300 hover:scale-105 m-2 flex items-center justify-center bg-zinc-900"
+          >
+            <Link href={`/portfolio/${dir}`} className="text-blue-500 text-2xl">
               {dir}
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </Layout>
   );
 };
